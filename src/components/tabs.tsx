69759f624a92bcef41b31386774024249935e22b
altierawr <104657457+altierawr@users.noreply.github.com>
2026-01-15 04:04:16 +0200

    fix: pass along props in tabs component

--- src/components/tabs.tsx
+++ src/components/tabs.tsx
@@ -5,11 +5,12 @@ import styles from "./tabs.module.css"
 
 type TRootProps = ComponentProps<typeof Tabs.Root>
 
-const Root = ({ defaultValue, className, children }: TRootProps) => {
+const Root = ({ defaultValue, className, children, ...props }: TRootProps) => {
   return (
     <Tabs.Root
       defaultValue={defaultValue}
       className={clsx(className, styles.root)}
+      {...props}
     >
       {children}
     </Tabs.Root>
@@ -18,9 +19,9 @@ const Root = ({ defaultValue, className, children }: TRootProps) => {
 
 type TListProps = ComponentProps<typeof Tabs.List>
 
-const List = ({ className, children }: TListProps) => {
+const List = ({ className, children, ...props }: TListProps) => {
   return (
-    <Tabs.List className={clsx(className, styles.list)}>
+    <Tabs.List className={clsx(className, styles.list)} {...props}>
       <>
         {children}
         <Tabs.Indicator className={styles.indicator} />
@@ -31,9 +32,9 @@ const List = ({ className, children }: TListProps) => {
 
 type TTabProps = ComponentProps<typeof Tabs.Tab>
 
-const Tab = ({ value, className, children }: TTabProps) => {
+const Tab = ({ value, className, children, ...props }: TTabProps) => {
   return (
-    <Tabs.Tab value={value} className={clsx(className, styles.tab)}>
+    <Tabs.Tab value={value} className={clsx(className, styles.tab)} {...props}>
       {children}
     </Tabs.Tab>
   )
@@ -45,9 +46,13 @@ const TabSeparator = () => {
 
 type TPanelProps = ComponentProps<typeof Tabs.Panel>
 
-const Panel = ({ value, className, children }: TPanelProps) => {
+const Panel = ({ value, className, children, ...props }: TPanelProps) => {
   return (
-    <Tabs.Panel value={value} className={clsx(className, styles.panel)}>
+    <Tabs.Panel
+      value={value}
+      className={clsx(className, styles.panel)}
+      {...props}
+    >
       {children}
     </Tabs.Panel>
   )
