4e0ba21014dadc735ba552e29667c3dd3e5a268d
altierawr <104657457+altierawr@users.noreply.github.com>
2026-01-18 18:31:30 +0200

    feat: add menu component

---
+++ src/components/menu.stories.tsx
@@ -0,0 +1,38 @@
+import { Meta, StoryObj } from "@storybook/react-vite"
+import { Button } from "./button"
+import * as Menu from "./menu"
+
+const meta = {
+  component: Menu.Root,
+  title: "Menu",
+  tags: ["autodocs"],
+} satisfies Meta<typeof Menu.Root>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <div className="dark">
+        <Menu.Root>
+          <Menu.Trigger render={<Button variant="solid" color="blue" />}>
+            Open Menu
+          </Menu.Trigger>
+
+          <Menu.Content>
+            <Menu.Item>Play Next</Menu.Item>
+            <Menu.Item>Play Last</Menu.Item>
+            <Menu.Separator />
+            <Menu.Item>Add to Library</Menu.Item>
+            <Menu.Item>Add to Playlist</Menu.Item>
+            <Menu.Separator />
+            <Menu.Item>Favorite</Menu.Item>
+            <Menu.Item>Share</Menu.Item>
+          </Menu.Content>
+        </Menu.Root>
+      </div>
+    )
+  },
+}
---
+++ src/components/menu.tsx
@@ -0,0 +1,68 @@
+import { Menu as BaseMenu } from "@base-ui/react"
+import clsx from "clsx"
+import { ComponentProps, PropsWithChildren, ReactElement } from "react"
+import styles from "./menu.module.css"
+
+type TRootProps = ComponentProps<typeof BaseMenu.Root>
+
+const Root = ({ children, ...props }: TRootProps) => {
+  return (
+    <BaseMenu.Root data-accent-color="gray" {...props}>
+      {children}
+    </BaseMenu.Root>
+  )
+}
+
+type TTriggerProps = ComponentProps<typeof BaseMenu.Trigger>
+
+const Trigger = ({ children, ...props }: TTriggerProps) => {
+  return <BaseMenu.Trigger {...props}>{children}</BaseMenu.Trigger>
+}
+
+type TContentProps = PropsWithChildren
+
+const Content = ({ children }: TContentProps) => {
+  return (
+    <BaseMenu.Portal>
+      <BaseMenu.Positioner className={styles.positioner} sideOffset={8}>
+        <BaseMenu.Popup data-accent-color="gray" className={styles.popup}>
+          {children}
+        </BaseMenu.Popup>
+      </BaseMenu.Positioner>
+    </BaseMenu.Portal>
+  )
+}
+
+type TArrowProps = ComponentProps<typeof BaseMenu.Arrow>
+
+const Arrow = ({ children, ...props }: TArrowProps) => {
+  return <BaseMenu.Arrow {...props}>{children}</BaseMenu.Arrow>
+}
+
+type TItemProps = ComponentProps<typeof BaseMenu.Item>
+
+const Item = ({ className, children, ...props }: TItemProps) => {
+  return (
+    <BaseMenu.Item
+      className={clsx(className, styles.item)}
+      data-accent-color="gray"
+      {...props}
+    >
+      {children}
+    </BaseMenu.Item>
+  )
+}
+
+type TSeparatorProps = ComponentProps<typeof BaseMenu.Separator>
+
+const Separator = ({ className, ...props }: TSeparatorProps) => {
+  return (
+    <BaseMenu.Separator
+      className={clsx(className, styles.separator)}
+      data-accent-color="gray"
+      {...props}
+    />
+  )
+}
+
+export { Root, Trigger, Content, Arrow, Item, Separator }
