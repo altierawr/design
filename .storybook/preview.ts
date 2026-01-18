14920277be1157fc000a8eb6f40eecead3282597
altierawr <104657457+altierawr@users.noreply.github.com>
2026-01-18 18:31:12 +0200

    feat: add dark mode support to storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,16 +1,29 @@
 import type { Preview } from "@storybook/react-vite"
+import { create, themes } from "storybook/theming"
 import "../src/styles/index.css"
 import "../src/styles/tokens/colors/blue.css"
 import "../src/styles/tokens/colors/gray.css"
-import "./main.css"
+import "./preview.css"
 
 const preview: Preview = {
   parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
+    backgrounds: {
+      disable: true,
+    },
+    darkMode: {
+      dark: {
+        ...create({
+          base: "dark",
+        }),
       },
+      light: {
+        ...create({
+          base: "light",
+        }),
+      },
+      classTarget: "body",
+      darkClass: "dark",
+      stylePreview: true,
     },
   },
 }
