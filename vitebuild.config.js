28a42c669f730418159c2fdabee2b477b8e5536f
altierawr <104657457+altierawr@users.noreply.github.com>
2026-02-03 16:16:44 +0200

    chore: update peer deps

--- vitebuild.config.js
+++ vitebuild.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     },
     rollupOptions: {
       // Remove these deps from the bundled library
-      external: ["react", "react/jsx-runtime", "react-dom"],
+      external: ["react", "react-dom"],
     },
   },
 })
