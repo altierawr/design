5d7d2dd8c6d25866544c8ab56c294e370a3f648b
altierawr <104657457+altierawr@users.noreply.github.com>
2026-02-08 19:50:20 +0200

    chore: optimize bundle size by externalizing deps & separating css to each component

--- vitebuild.config.js
+++ vitebuild.config.js
@@ -1,17 +1,43 @@
-import { resolve } from "path";
+import { glob } from "glob";
+import { fileURLToPath } from "node:url";
+import { resolve, extname, relative } from "path";
 import { defineConfig } from "vite";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+
+import packageJson from "./package.json";
 
 export default defineConfig({
+  plugins: [libInjectCss()],
   build: {
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "src/index.ts"),
-      name: "Lib",
-      fileName: "index",
+      formats: ["es"],
     },
     rollupOptions: {
-      // Remove these deps from the bundled library
-      external: ["react", "react-dom"],
+      external: [
+        "react",
+        "react-dom",
+        "react/jsx-runtime",
+        ...Object.keys(packageJson.dependencies || {}),
+        ...Object.keys(packageJson.peerDependencies || {}),
+      ],
+      input: Object.fromEntries(
+        glob
+          .sync("src/**/*.{ts,tsx}", {
+            ignore: ["src/**/*.d.ts", "src/**/*.stories.tsx", "src/styles/**"],
+          })
+          .map((file) => [
+            // The name of the entry point
+            // lib/nested/foo.ts becomes nested/foo
+            relative("src", file.slice(0, file.length - extname(file).length)),
+            // The absolute path to the entry file
+            fileURLToPath(new URL(file, import.meta.url)),
+          ]),
+      ),
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
     },
   },
 });
